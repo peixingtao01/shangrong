8a3d3fe219f16d94c331c243fbae7c1e157c8715
peixingtao01 <1307128051@qq.com>
2020-03-28 22:00:07 +0800

    3.28 3.28-2

--- JSSR/DeskShow/views.py
+++ JSSR/DeskShow/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+from django.shortcuts import HttpResponseRedirect
+import time
+import hashlib
 # Create your views here.
+
+def index(request):
+    return render(request,'DeskShow/index.html',locals())
